ec65dd4c40a33bb77a8a0b05f2587655f5704551
jambon-beurre <fe-lix@hotmail.fr>
2015-10-05 17:58:21 +0200

    Connexion qui marche

--- Begin.php
+++ Begin.php
@@ -5,14 +5,18 @@ function Connect_db(){
 	$password="212840";
 	$bdname="p1403498";
 	try{
-		$bdd=new PDO('mysql:host='.$host.';dbname='.$bdname.
+		$bdd = new PDO('mysql:host='.$host.';dbname='.$bdname.
 			';charset=utf8',$user,$password);
 		return $bdd;
 	}
-	catch(Exception $e){
+	catch(Exception $e)
+	{
 		die('Erreur: ' .$e->getMessage());
 	}
 }
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +37,21 @@ function Connect_db(){
 		<?php
 
 		$connected = false;
-		Connect_db();
+		
+		if (isset($_GET['email']) && isset($_GET['password']))
+		{			
+			$bdd = Connect_db();
+			$query = $bdd->prepare('SELECT email, password FROM Membre');
+			$query->execute(); 
+			while ($line = $query->fetch()) //Si il l'est déja, on modifie
+			{
+				if ($line['email'] == $_GET['email'] && $line['password'] == $_GET['password'])
+				{
+					$connected = true;
+					break;
+				}
+			}
+		}
 
 		if ($connected){
 			echo '<a href="profil.php">Profil</a>';
